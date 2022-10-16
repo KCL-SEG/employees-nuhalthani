708a00d910121ceda7e877701c258b97d3b7eb42
Noha Althani <noha.althani@kcl.ac.uk>
2022-10-16 23:17:04 +0100

    solved

--- employee.py
+++ employee.py
@@ -2,14 +2,78 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, salary, contract,commission, commission_type, commission_total, contract_total, hours_worked):
         self.name = name
+        self.salary = salary
+        self.contract = contract
+        self.commission = commission
+        self.commission_type = commission_type
+        self.commission_total = commission_total
+        self.contract_total = contract_total
+        self.hours_worked = hours_worked
 
     def get_pay(self):
-        pass
+        payment = 0
+        if self.contract == "monthly":
+            payment = get_monthly(self)
+        else:
+            payment = get_hourly(self)
+
+        return payment
+
+    def get_monthly(self):
+        payment = 0
+        if self.commission == False:
+            payment = self.salary
+        elif self.commission and self.commission_type == "bonus":
+            payment = self.salary + self.commission_total
+        else:
+            payment = self.salary + (self.contract_total * self.commission_total)
+
+        return payment
+
+    def get_hourly(self):
+        payment = 0
+        if self.commission == False:
+            payment = self.salary * self.hours_worked
+        elif self.commission and self.commission_type == "bonus":
+            payment = (self.salary * self.hours_worked) + self.commission_total
+        else:
+            payment = (self.salary * self.hours_worked) + (self.contract_total * self.commission_total)
+
+        return payment
 
     def __str__(self):
-        return self.name
+        final_string = ""
+
+        if self.contract == "monthly":
+            final_string = monthly_string(self)
+        else:
+            final_string = hourly_string(self)
+
+        return final_string
+
+    def monthly_string(self):
+        final_string = ""
+        if self.commission == False:
+            final_string += f'{self.name} works on a monthly salary of {self.salary}. Their total pay is {self.get_pay()}.'
+        elif self.commission and self.commission_type == "bonus":
+            final_string += f'{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.commission_total}. Their total pay is {self.get_pay()}.'
+        else:
+            final_string += f'{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.contract_total} contract(s) at {self.commission_total}/contract. Their total pay is {self.get_pay()}.'
+
+        return final_string
+
+    def hourly_string(self):
+        final_string = ""
+        if self.commission == False:
+            final_string += f'{self.name} works on a contract of {self.salary} hours at {self.hours_worked}/hour. Their total pay is {self.get_pay()}.'
+        elif self.commission and self.commission_type == "bonus":
+            final_string += f'{self.name} works on a contract of {self.salary} hours at {self.hours_worked}/hour and receives a bonus commission of {self.commission_total}. Their total pay is {self.get_pay()}.'
+        else:
+            final_string += f'{self.name} works on a contract of {self.salary} hours at {self.hours_worked}/hour and receives a commission for {self.contract_total} contract(s) at {self.commission_total}/contract. Their total pay is {self.get_pay()}.'
+
+        return final_string
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
